ad60937d98cec7d39724f1fabb59460e4ef1bac6
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-05-25 16:36:05 +0530

    Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./pages/Footer";
+
 import HomePage from "./pages/HomePage";
+import PastEdition from "./pages/Past-Editions";
+import EditionOverview from "./pages/PastEvent-Detail";
+import AudienceProfile from "./pages/AudienceProfile";
+import AboutUs from "./pages/About-Us";
+import OfficialMessage from "./pages/Official-Message";
+import Cart from "./components/Cart";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+    <ScrollToTop/>
+
       <Navbar />
-      <HomePage />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/past-edition" element={<PastEdition />} />
+        <Route path="/past-event-detail" element={<EditionOverview />} />
+        <Route path="/audience-profile"  element={<AudienceProfile/>} />
+        <Route path="/about-us"  element={<AboutUs/>} />
+        <Route path="/official-message"  element={<OfficialMessage/>} />
+        <Route path="/cart"  element={<Cart/>} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
