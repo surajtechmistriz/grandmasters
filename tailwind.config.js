e82d77daf689db12e198068eead08a9705b529da
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-05-25 16:33:30 +0530

    font-family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['"PT Sans Caption"', "Roboto"],
+        roboto: ["Roboto"],
+      },
+
+      colors: {
+        brandRed: "rgb(208, 37, 45)",
+      },
+
       keyframes: {
         fade: {
-          '0%, 100%': { opacity: '0.2' },
-          '50%': { opacity: '1' },
+          "0%, 100%": {
+            opacity: "0.2",
+          },
+          "50%": {
+            opacity: "1",
+          },
         },
       },
+
       animation: {
-        fade: 'fade 1.5s infinite',
+        fade: "fade 1.5s infinite",
       },
     },
   },
+
   plugins: [],
-}
+};
